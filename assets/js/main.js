81c6765d60c4188da96a98ae39d60492a163d626
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-08-20 23:54:12 -0700

    Simplify dark-light theme switcher and CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,82 +1,75 @@
 const toggleThemeBtn = document.getElementById("toggleTheme");
-const toggleLogo = document.getElementById("logo")
-const gitHubLogoList = document.getElementsByClassName("gh")
-const metaLogoList = document.getElementsByClassName("meta")
-const mozLogo = document.getElementById("moz")
-const samLogo = document.getElementById("sam")
-const currentTheme = localStorage.getItem("theme");
-const sysIsDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const toggleLogo = document.getElementById("logo");
+const gitHubLogoList = document.getElementsByClassName("gh");
+const metaLogoList = document.getElementsByClassName("meta");
+const mozLogo = document.getElementById("moz");
+const samLogo = document.getElementById("sam");
 
-function getSavedTheme(savedTheme) {
-    if (localStorage.getItem("overRideSysColour") === "true") {
-        document.body.classList.remove('systemDarkPreference');
-        document.body.classList.toggle(savedTheme);
-    }
+function getCurrentTheme() {
+  return (
+    document.body.dataset.theme ||
+    localStorage.getItem("theme") ||
+    (window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light")
+  );
 }
 
-function toggleTheme(currentSysIsDark) {
-    const hasSystemDarkClass = document.body.classList.contains('systemDarkPreference');
-    const isDark = (hasSystemDarkClass && currentSysIsDark) || document.body.classList.contains('dark');
+function setTheme() {
+  const theme = getCurrentTheme();
 
-    document.body.classList.remove('systemDarkPreference');
-    document.body.classList.toggle('dark', !isDark);
-}
+  if (theme === "dark") {
+    assignDarkThemeEl();
+  } else {
+    assignLightThemeEl();
+  }
 
-function getThemeBtnText(theme, currentSysIsDark) {
-    if((theme === "dark") || (currentSysIsDark && theme !== "light")){
-        assignDarkThemeEl();
-    } else {
-        assignLightThemeEl();
-    }
-}
-
-function setAndSaveTheme() {
-    let theme;
-    if (document.body.classList.contains("dark")) {
-        theme = "dark";
-        assignDarkThemeEl();
-    } else {
-        theme = "light";
-        assignLightThemeEl();
-    }
-    localStorage.setItem("theme", theme);
-    localStorage.setItem("overRideSysColour", "true")
+  document.body.dataset.theme = theme;
+  localStorage.setItem("theme", theme);
 }
 
 function assignDarkThemeEl() {
-    toggleThemeBtn.innerText = "Go Light 🌞"
-    toggleLogo.src = "/img/webdocs_logo_dark.svg"
-    if(window.location.pathname == "/team/"){
-        mozLogo.src = "/img/mozilla-light.png"
-        samLogo.src = "/img/samsung-light.png"
-    }
-    for(let i in gitHubLogoList){
-        gitHubLogoList[i].src = "/img/github-light.png"
-    }
-    for(let i in metaLogoList){
-        metaLogoList[i].src = "/img/meta-dark.svg"
-    }
+  toggleThemeBtn.innerText = "Go Light 🌞";
+  toggleLogo.src = "/img/webdocs_logo_dark.svg";
+  if (window.location.pathname == "/team/") {
+    mozLogo.src = "/img/mozilla-light.png";
+    samLogo.src = "/img/samsung-light.png";
+  }
+  for (let i in gitHubLogoList) {
+    gitHubLogoList[i].src = "/img/github-light.png";
+  }
+  for (let i in metaLogoList) {
+    metaLogoList[i].src = "/img/meta-dark.svg";
+  }
 }
 
 function assignLightThemeEl() {
-    toggleThemeBtn.innerText = "Go Dark 🌚"
-    toggleLogo.src = "/img/webdocs_logo_light.svg"
-    if(window.location.pathname == "/team/"){
-        mozLogo.src = "/img/mozilla-dark.png"
-        samLogo.src = "/img/samsung-dark.png"
-    }
+  toggleThemeBtn.innerText = "Go Dark 🌚";
+  toggleLogo.src = "/img/webdocs_logo_light.svg";
+  if (window.location.pathname == "/team/") {
+    mozLogo.src = "/img/mozilla-dark.png";
+    samLogo.src = "/img/samsung-dark.png";
+  }
 
-    for(let i in gitHubLogoList){
-        gitHubLogoList[i].src = "/img/github-dark.png"
-    }
-    for(let i in metaLogoList){
-        metaLogoList[i].src = "/img/meta-light.svg"
-    }
+  for (let i in gitHubLogoList) {
+    gitHubLogoList[i].src = "/img/github-dark.png";
+  }
+  for (let i in metaLogoList) {
+    metaLogoList[i].src = "/img/meta-light.svg";
+  }
 }
 
-getSavedTheme(currentTheme);
-getThemeBtnText(currentTheme, sysIsDark);
+// Set the data-theme attr and update local storage
+setTheme();
+
 toggleThemeBtn.addEventListener("click", function () {
-    toggleTheme(sysIsDark);
-    setAndSaveTheme();
+  const isDark = document.body.dataset.theme == "dark";
+  document.body.dataset.theme = isDark ? "light" : "dark";
+
+  setTheme();
 });
+
+setTimeout(() => {
+  // Prevent flash of dark while switching to light theme variables
+  document.body.classList.add("loaded");
+}, 1);
