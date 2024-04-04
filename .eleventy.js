7c7355d6925dad29c51dec91bf0f0639c97ebeb5
Florian Scholz <fs@florianscholz.com>
2024-04-04 14:00:21 +0200

    Run prettier; fix year in footer; update gc description

--- .eleventy.js
+++ .eleventy.js
@@ -64,6 +64,8 @@ module.exports = function (eleventyConfig) {
     },
   });
 
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
   return {
     dir: {
       input: "pages",
